121d8fca2a8a615da998c97ebd067f1373efe242
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-07-18 21:35:35 -0400

    Setup relationships

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,3 +1,26 @@
 class LineItem < ActiveRecord::Base
-  attr_accessible :note, :product_id, :quantity
+  belongs_to :product
+  belongs_to :truck
+  
+  accepts_nested_attributes_for :product
+  attr_accessible :note, :product_id, :quantity, :product_id, :truck_id
+  
+  after_initialize :after_init
+  
+  def product_attributes=(attributes)
+    p = Product.find_by_make_and_model attributes[:make], attributes[:model]
+    if p.nil?
+      build_product
+      self.product.attributes = attributes
+    else
+      self.product = p
+    end
+  end
+  
+  protected
+  
+  def after_init
+    build_product if product.nil?
+  end
+
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,4 @@
 class Truck < ActiveRecord::Base
+  has_many :line_items
   attr_accessible :name, :region
 end
