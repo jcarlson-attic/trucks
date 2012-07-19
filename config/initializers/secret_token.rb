e68f713251acddcd821492c6ec85d7eed87e59f7
Jarrod Carlson <jarrod.carlson@gmail.com>
2012-07-18 21:25:27 -0400

    Initial code

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trucks::Application.config.secret_token = '474ae25da9219234d63a0f6b27869cef33a4d6105d7c4562d76963034c8e22b7247b8a90070dffdf4dc2354e6ff193abdb3026a4810c4d31b9e28eeb9f2c6f9b'
